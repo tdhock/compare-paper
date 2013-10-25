f1ba46f22f4da3b87b29a7734d45211126d0ea79
Toby Dylan Hocking <toby@sg.cs.titech.ac.jp>
2013-10-25 12:03:03 +0900

    roc/auc calc

--- figure-simulation-proportion.R
+++ figure-simulation-proportion.R
@@ -1,15 +1,77 @@
-works_with_R("3.0.2", plyr="1.8", reshape2="1.2.2")
+works_with_R("3.0.2", plyr="1.8", reshape2="1.2.2", rankSVMcompare="2013.10.25")
 
 source("tikz.R")
 source("colors.R")
 
+library(grid)
+
 load("simulation.proportion.RData")
 
+leg <- "learned\nfunction"
+classify <- function(x, thresh=1){
+  stopifnot(is.numeric(thresh))
+  stopifnot(is.finite(thresh))
+  stopifnot(thresh >= 0)
+  stopifnot(length(thresh) == 1)
+  ifelse(x > thresh, 1L,
+         ifelse(x < -thresh, -1L, 0L))
+}
+tr <- simulation.proportion$test.rank
+roc <- data.frame()
+auc <- data.frame()
+for(prop in names(tr)){
+  seed.list <- tr[[prop]]
+  for(seed in names(seed.list)){
+    fit.list <- seed.list[[seed]]
+    norm <- "linf"
+    for(fit.name in names(fit.list)){
+      rank.mat <- fit.list[[fit.name]]
+      rank.diff <- rank.mat[,2]-rank.mat[,1]
+      thresh.vec <- c(0, sort(abs(rank.diff)))
+      Pairs <- simulation.proportion$data[[prop]][[seed]][[norm]]$test
+      this.roc <- data.frame()
+      info <- data.frame(fit.name, norm,
+                         seed=as.integer(seed),
+                         prop=as.numeric(prop))
+      for(thresh in thresh.vec){
+        yhat <- classify(rank.diff, thresh)
+        err <- FpFnInv(Pairs$yi, yhat)
+        this.roc <- rbind(this.roc, data.frame(thresh, info, err))
+      }
+      this.roc$FPR <- with(this.roc, false.positive/equality)
+      this.roc$FNR <- with(this.roc, false.negative/inequality)
+      this.roc$TPR <- 1-this.roc$FNR
+      boxes <- ddply(this.roc, .(TPR), summarize, min=min(FPR), max=max(FPR))
+      ## ggplot()+
+      ##   geom_path(aes(FPR, TPR), data=this.roc)+
+      ##   geom_segment(aes(min,TPR,xend=max,yend=TPR),
+      ##                data=boxes,colour="red",lwd=2)
+      auc <- rbind(auc, data.frame(info, auc=sum(with(boxes, (max-min)*TPR))))
+      roc <- rbind(roc, this.roc)
+    }
+  }
+}
+ggplot(roc, aes(FPR, TPR))+
+  geom_path(aes(colour=fit.name, group=interaction(fit.name, seed)))+
+  facet_wrap("prop")+
+  theme_bw()+
+  theme(panel.margin=unit(0,"cm"))
+auc.stats <- ddply(auc, .(fit.name, norm, prop), summarize,
+                   mean=mean(auc), sd=sd(auc))
+ggplot(auc.stats, aes(prop, mean))+
+  geom_ribbon(aes(fill=fit.name,ymin=mean-sd,ymax=mean+sd), alpha=1/4)+
+  geom_line(aes(colour=fit.name),lwd=2)+
+  facet_grid(.~norm)+
+  theme_bw()+
+  theme(panel.margin=unit(0,"cm"))+
+  xlab("proportion of equality pairs")+
+  scale_colour_manual(leg,values=model.colors)+
+  scale_fill_manual(leg,values=model.colors)+
+  ylab("Area under the ROC curve")
 ## matrix versions of the norm.
 funs <- list(l2=function(x)rowSums(x*x),
              l1=function(x)rowSums(abs(x))^2,
              linf=function(x)apply(abs(x), 1, max)^2)
-
 size.list <- simulation.proportion$data
 err <- simulation.proportion$err
 err$percent <- err$error / err$count * 100
@@ -34,8 +96,7 @@ for(set.id in sets$set.id){
   test <- set.list$test
   fun <- funs[[norm]]
   fxdiff <- with(test, fun(Xip)-fun(Xi))
-  yhat <- ifelse(fxdiff > 1, 1L,
-                 ifelse(fxdiff < -1, -1L, 0))
+  yhat <- classify(fxdiff)
   table(yhat, test$yi)
   percent <- mean(yhat != test$yi) * 100
   bayes.df <- rbind(bayes.df, data.frame(info, percent))
@@ -69,7 +130,6 @@ components <- melt(err, id.vars=c("prop","seed","norm","fit.name"),
                    measure.vars=c("false.positive","false.negative"))
 comp.sum <- ddply(components, .(prop,norm,fit.name,variable), summarize,
                   mean=mean(value), sd=sd(value))
-library(grid)
 ggplot(comp.sum, aes(prop, mean, group=interaction(fit.name, variable)))+
   geom_ribbon(aes(fill=fit.name,ymin=mean-sd,ymax=mean+sd), alpha=1/4)+
   geom_line(aes(colour=fit.name),lwd=2)+
@@ -85,7 +145,6 @@ labels <- c(l1="||x||_1^2",
 makelabel <- function(x)sprintf("$r(x) = %s$", labels[as.character(x)])
 percents$label <- makelabel(percents$norm)
 err$label <- makelabel(err$norm)
-leg <- "learned\nfunction"
 boring <- ggplot(percents, aes(prop*100, mean, group=fit.name))+
   geom_ribbon(aes(ymin=mean-sd,ymax=mean+sd,fill=fit.name),alpha=1/2)+
   geom_line(aes(colour=fit.name),lwd=1.5)+
