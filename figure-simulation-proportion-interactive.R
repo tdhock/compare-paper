a1750a265615344fde52f542e6f7e3bcd7df6725
Toby Dylan Hocking <toby@sg.cs.titech.ac.jp>
2013-10-28 14:58:10 +0900

    interactive auc

--- figure-simulation-proportion-interactive.R
+++ figure-simulation-proportion-interactive.R
@@ -4,6 +4,7 @@ source("tikz.R")
 source("colors.R")
 
 load("simulation.proportion.RData")
+load("simulation.roc.RData")
 
 ## matrix versions of the norm.
 funs <- list(l2=function(x)rowSums(x*x),
@@ -21,16 +22,16 @@ err$percent <- err$error / err$count * 100
 sets <- dcast(err, prop + seed + norm ~ fit.name, value.var="percent")
 sets$diff <- sets$compare-sets$rank2
 sets$set.id <- 1:nrow(sets)
-diff.df <- ddply(sets, .(prop, norm), summarize,
-                   prop=prop[1], norm=norm[1],
-                   mean=mean(diff), sd=sd(diff))
 train.df <- data.frame()
 bayes.df <- data.frame()
+auc <- simulation.roc$auc
+auc$set.id <- NA
 for(set.id in sets$set.id){
   e <- sets[set.id,]
   prop <- as.character(e$prop)
   norm <- as.character(e$norm)
   seed <- as.character(e$seed)
+  auc$set.id[auc$norm == norm & auc$prop == prop & auc$seed == seed] <- set.id
   err$set.id[err$norm == norm & err$prop == prop & err$seed == seed] <- set.id
   rank.in.set <-
     rank.df$norm == norm & rank.df$prop == prop & rank.df$seed == seed
@@ -84,7 +85,8 @@ leg <- "learned\nfunction"
 xl <- xlab("feature 1")
 yl <- ylab("feature 2")
 x.lab <- "proportion of equality pairs"
-ord <- c("latent","compare","rank2", "rank")
+x.sc <- scale_x_continuous(breaks=seq(0.1, 0.9, by=0.2))
+ord <- c("rank", "rank2", "compare", "truth", "latent")
 bayes.df$model <- factor("latent",ord)
 err$fit.name <- factor(err$fit.name, ord)
 sets$Xt.1 <- 0
@@ -103,34 +105,40 @@ viz <-
        xl+yl+
        ggtitle("training data"),
        error=ggplot()+
-       make_text(err, 1/2, 22, "norm")+
-       geom_point(aes(prop, percent, colour=model, size=model,
-                      showSelected=norm, showSelected2=set.id),
-                  data=bayes.df)+
-       geom_point(aes(prop, percent, colour=fit.name, colour=fit.name,
+       make_text(err, 1/2, 1, "norm")+
+       geom_point(aes(prop, auc, colour=fit.name, size=fit.name,
+                      showSelected=set.id),
+                  data=subset(auc,fit.name=="truth"))+
+       geom_point(aes(prop, auc, colour=fit.name, size=fit.name,
                       showSelected=norm,
                       clickSelects=set.id),
-                 lwd=3,alpha=3/4,data=err)+
-       ylab("percent incorrectly predicted test pairs")+
+                 alpha=7/10,data=subset(auc,fit.name!="truth"))+
+       ylab("Area under the test ROC curve")+
        scale_colour_manual("model", values=model.colors, breaks=ord)+
        scale_size_manual("model",
-                         values=c(rank2=1, rank=1, compare=1, latent=3)*2,
+                         values=c(rank2=1, rank=1, compare=1,
+                           latent=3, truth=3)*3,
                          breaks=ord)+
-       xlab(x.lab)+
-       ggtitle("test error, select data set"),
-       diff=ggplot()+
-       geom_ribbon(aes(prop, ymin=mean-sd, ymax=mean+sd, group=norm,
-                       clickSelects=norm), alpha=1/2,
-                   data=diff.df)+
-       geom_line(aes(prop, mean, group=norm, clickSelects=norm), 
-                   data=diff.df)+
-       geom_hline(yintercept=0, color="red")+
-       geom_text(aes(x,y,label=label),color="red",
-                 data=data.frame(x=0.5,y=1,label="no difference"))+
-       ggtitle("test error difference, select norm")+
-       xlab(x.lab)+
-       ylab("<- compare better (test error percent difference) rank better->"))
-for(model in c("compare", "rank", "rank2")){
+       x.sc+xlab(x.lab)+
+       ggtitle("test AUC, select data set"))
+## difference... of AUC.
+wide.auc <- dcast(auc,norm+seed+prop~fit.name,value.var="auc")
+wide.auc$diff <- with(wide.auc, rank-compare)
+diff.df <- ddply(wide.auc, .(prop, norm), summarize,
+                 mean=mean(diff), sd=sd(diff))
+viz$diff <- ggplot()+
+  geom_hline(yintercept=0, color="red")+
+  geom_text(aes(x,y,label=label),color="red",
+            data=data.frame(x=0.5,y=0.005,label="no difference"))+
+  geom_ribbon(aes(prop, ymin=mean-sd, ymax=mean+sd, group=norm,
+                  clickSelects=norm), alpha=1/2,
+              data=diff.df)+
+  geom_line(aes(prop, mean, group=norm, clickSelects=norm), 
+            data=diff.df)+
+  ggtitle("test AUC difference, select norm")+
+  x.sc+xlab(x.lab)+
+  ylab("<- compare better (test AUC difference) rank better->")
+for(model in c("compare", "rank2", "rank")){
   ## sub.df <- subset(rank.df, what==model)
   ## latent.df <- subset(rank.df, what=="latent" & seed==seed[1] & prop==prop[1])
   sub.df <- subset(rank.df, what %in% c(model, "latent"))
