145165a05b2dea387fcdd4cd7a3eac8dc184dda8
David Venuto <dv.venuto@gmail.com>
2017-12-07 15:57:49 -0500

    Updates

--- chess_plot.R
+++ chess_plot.R
@@ -47,7 +47,7 @@ four_AUC[four_AUC$kern == "Vanilla",2] <- "Linear SVM"
 four_AUC[four_AUC$kern == "rbf",2] <- "Gaussian SVM"
 four_AUC[four_AUC$kern == "polydot",2] <- "Poynomial SVM"
 four_AUC[four_AUC$ftrs == "aftrs",4] <- "16 (All Features)"
-four_AUC[four_AUC$ftrs == "2ftrs",4] <- "2 (Only Glicko and ELO Score) "
+four_AUC[four_AUC$ftrs == "2ftrs",4] <- "2 (Only Glicko and ELO scr) "
 four_AUC[four_AUC$kern == "Baseline",4] <- "0"
 four_AUC[four_AUC$kern == "ELO",4] <- "1"
 four_AUC[four_AUC$kern == "Glicko",4] <- "1"
@@ -69,12 +69,12 @@ colnames(four_AUC)[2] <- "Features"
 
 four_AUC <- four_AUC[!duplicated(four_AUC),]
 
-four_AUC$Features <- factor(four_AUC$Features,levels = c("0","1","2 (Only Glicko and ELO Score) ","16 (All Features)"))
+four_AUC$Features <- factor(four_AUC$Features,levels = c("0","1","2 (Only Glicko and ELO scr) ","16 (All Features)"))
 
 p <- ggplot(four_AUC,aes(x=reorder(as.factor(kern),Median),y=Final_AUC,fill=Features)) + geom_boxplot(outlier.shape = NA)+ geom_point(shape=21, aes(y = Final_AUC,color=Features),position=position_dodge(width = .75))  + coord_flip() + xlab("Model") + ylab("AUC") + theme_bw() + theme(legend.position="bottom") + theme(legend.key.size = unit(1,"line")) + theme(text=element_text(size = 9)) + scale_color_manual(values=c("#ffffff","#000000","#000000","#000000")) + scale_fill_manual(values=c("#000000","#4c4ca6","#56B4E9","#ffffff"))
 
 library(tikzDevice)
 
-tikz("chess_fig.tex", width=5.5, height=3.5,)
+tikz("chess_fig.tex", width=4.75, height=3.15,)
 p 
 dev.off()
